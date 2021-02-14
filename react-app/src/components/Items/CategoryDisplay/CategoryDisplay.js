bce24016e09d7bc46afb797b717e452e3f0ce4e9
Kerri <gant.kerri@gmail.com>
2021-02-14 00:41:40 -0500

    filtered items are sorta working

--- react-app/src/components/Items/CategoryDisplay/CategoryDisplay.js
+++ react-app/src/components/Items/CategoryDisplay/CategoryDisplay.js
@@ -6,7 +6,7 @@ import './CategoryDisplay.css'
 
 
 const CategoryDisplay = () => {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     // const history = useHistory();
     const closetOwner = useSelector(state => state.user.closetOwner);
     const sectionCategory = useSelector(state => state.category.category)
@@ -17,23 +17,31 @@ const CategoryDisplay = () => {
     const [colors, setColors] = useState([]);
     const [styles, setStyles] = useState([]);
     const [weathers, setWeathers] = useState([]);
-    // const [subCateId, setSubCateId] = useState(null);
-    // const [colorId, setColorId] = useState(null);
-    // const [styleId, setStyleId] = useState(null);
-    // const [weatherId, setWeatherId] = useState(null);
+    const [subCateId, setSubCateId] = useState("-- Select One --");
+    const [colorId, setColorId] = useState("-- Select One --");
+    const [styleId, setStyleId] = useState(null);
+    const [weatherId, setWeatherId] = useState(null);
+
+    const [errors, setErrors] = useState([]);
 
     useEffect(() => {
         if (!sectionCategory) {
             return
         }
         setSubCate(sectionCategory.subCategories)
+        getColors()
         // setSubCateId(getIds(sectionCategory.subCategories))
-        // setColors()
-        // setStyles()
-        // setWeathers()
+        getStyles()
+        getWeather()
         setOriginalCategoryItems(allItems(sectionCategory.subCategories))
         setCategoryItems(allItems(sectionCategory.subCategories))
     }, []);
+
+    console.log("STYLINGS", styles)
+
+    useEffect(() => {
+        changeItemsViewed()
+    }, [subCateId, colorId, styleId, weatherId])
 
     const allItems = (subs) => {
         let all = []
@@ -43,49 +51,88 @@ const CategoryDisplay = () => {
         return all
     }
 
-    const getIds = (filters) => {
-        let allIds = [];
-        for (let i = 0; i < filters.length; i++) {
-            allIds.push(filters[i].id)
+    const getColors = async () => {
+        console.log("WE IN COLORS")
+        const response = await fetch("/api/items/colors")
+        if (response.ok) {
+            const theColors = await response.json()
+            return setColors(theColors.colors)
         }
-        return allIds
+    }
+    const getStyles = async () => {
+        console.log("WE IN COLORS")
+        const response = await fetch("/api/items/styles")
+        if (response.ok) {
+            const theStyles = await response.json()
+            return setStyles(theStyles.styles)
+        }
+    }
+    const getWeather = async () => {
+        console.log("WE IN COLORS")
+        const response = await fetch("/api/items/weather")
+        if (response.ok) {
+            const theWeather = await response.json()
+            console.log("WE GOT STYLES", theWeather)
+            return setWeathers(theWeather.weather)
+        }
     }
 
-    const handleChangeSubCate = (e) => {
-        // await setSubCateId()
-        console.log("Checking", e.target.value)
-        changeItemsViewed({ subCateId: e.target.value })
-    }
-    const handleChangeColor = (e) => {
-        // changeItemsViewed(_, e.target.value)
-    }
-    const handleChangeStyle = (e) => {
-        // changeItemsViewed(_, _, e.target.value)
-    }
-    const handleChangeWeather = (e) => {
-        // changeItemsViewed(_, _, _, e.target.value)
-    }
+    // const getIds = (filters) => {
+    //     let allIds = [];
+    //     for (let i = 0; i < filters.length; i++) {
+    //         allIds.push(filters[i].id)
+    //     }
+    //     return allIds
+    // }
+
+    // const handleChangeSubCate = (e) => {
+    //     setErrors([]);
+    //     return dispatch(setSubCateId(e.target.value))
+    //         .then((res) => subCateId)
+    //         .then(() => changeItemsViewed())
+    //         .catch((res) => {
+    //             if (res.data && res.data.errors) setErrors(res.data.errors);
+    //         })
+    //     // console.log("Maybe", subCateId)
+    //     // console.log("Okay", e.target.value)
+    //     // changeItemsViewed(e.target.value, colorId, styleId, weatherId)
+    // }
+    // console.log("Checking", subCateId)
+    // const handleChangeColor = (e) => {
+    //     // changeItemsViewed(_, e.target.value)
+    // }
+    // const handleChangeStyle = (e) => {
+    //     // changeItemsViewed(_, _, e.target.value)
+    // }
+    // const handleChangeWeather = (e) => {
+    //     // changeItemsViewed(_, _, _, e.target.value)
+    // }
     const handleChangeClean = (e) => { }
 
     // const changeItemsViewed = (subCateId, colors, weather, style) => {
 
-    const changeItemsViewed = ({ subCateId }) => {
+    const changeItemsViewed = () => {
         const itemsViewed = originalCategoryItems;
         const itemsReturned = []
         for (let i = 0; i < itemsViewed.length; i++) {
-            console.log("Working", itemsViewed[i].subCategoryId)
-            if (itemsViewed[i].subCategoryId == subCateId) {
+            if (subCateId && itemsViewed[i].subCategoryId == subCateId) {
+                itemsReturned.push(itemsViewed[i])
+            } else if (colorId && itemsViewed[i].colorId == colorId) {
+                itemsReturned.push(itemsViewed[i])
+            } else if (styleId && itemsViewed[i].styleId == styleId) {
+                itemsReturned.push(itemsViewed[i])
+            } else if (weatherId && itemsViewed[i].weatherId == weatherId) {
                 itemsReturned.push(itemsViewed[i])
             }
         }
-        console.log("These Items", subCateId)
-        if ((subCateId === '- - Select One - -')) {
+        if ((subCateId === '- - Select One - -') && (colorId === '- - Select One - -')) {
             return setCategoryItems(originalCategoryItems)
         }
-        return setCategoryItems(itemsReturned)
+        // return setCategoryItems(itemsReturned)
     }
 
-    console.log("Lets see", categoryItems)
+
+    console.log("SOME ITEMS", categoryItems)
 
     return (
         <div className="category-display-modal">
@@ -102,31 +149,34 @@ const CategoryDisplay = () => {
                         ))}
                     </ul>
                 </div >
-                <form className="category-display_filters">
+                <form className="category-display_filters" >
                     <div>Filter Items</div>
                     <label>Sub-Category:</label>
-                    <select id='Sub-Category' onChange={handleChangeSubCate}>
+                    <select id='Sub-Category' onChange={(e) => setSubCateId(e.target.value)}>
                         <option>- - Select One - -</option>
                         {subCate.map(subCat => (
                             <option value={subCat.id}>{subCat.subCategoryName}</option>
                         ))}
                     </select>
                     <label>Color:</label>
-                    <select onChange={handleChangeColor}>
+                    <select onChange={(e) => setColorId(e.target.value)}>
+                        <option>- - Select One - -</option>
                         {colors.map(color => (
-                            <option>{color}</option>
+                            <option value={color.id}>{color.color}</option>
                         ))}
                     </select>
                     <label>Style:</label>
-                    <select onChange={handleChangeStyle}>
+                    <select onChange={(e) => setStyleId(e.target.value)}>
+                        <option>- - Select One - -</option>
                         {styles.map(style => (
-                            <option>{style}</option>
+                            <option value={style.id}>{style.styleType}</option>
                         ))}
                     </select>
                     <label>Weather:</label>
-                    <select onChange={handleChangeWeather}>
+                    <select onChange={(e) => setWeatherId(e.target.value)}>
+                        <option>- - Select One - -</option>
                         {weathers.map(weather => (
-                            <option>{weather}</option>
+                            <option value={weather.id}>{weather.weatherType}</option>
                         ))}
                     </select>
                     <label>Clean Only
