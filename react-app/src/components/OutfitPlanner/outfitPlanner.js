9d71ed37378b77ceb8f32f183234b48f1af8c9d0
Kerri <gant.kerri@gmail.com>
2021-03-30 20:20:55 -0400

    fixed teaser

--- react-app/src/components/OutfitPlanner/outfitPlanner.js
+++ react-app/src/components/OutfitPlanner/outfitPlanner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
+import theDoor from '../../images/theGreenestDoor.jpg';
 
 
 import './outfitPlanner.css'
@@ -8,8 +9,28 @@ import './outfitPlanner.css'
 
 const OutfitPlanner = (items) => {
     return (
-        <div>
-            Coming Soon ...
+        <div className="planner-main">
+            <div className="planner-header">
+                Coming Soon ...
+            </div>
+            <div>
+                <img alt="Closet Door" src={theDoor} className="closet-door-left30" />
+            </div>
+            <div>
+                <img alt="Closet Door" src={theDoor} className="closet-door-left20" />
+            </div>
+            <div>
+                <img alt="Closet Door" src={theDoor} className="closet-door-left10" />
+            </div>
+            <div>
+                <img alt="Closet Door" src={theDoor} className="closet-door-right30" />
+            </div>
+            <div>
+                <img alt="Closet Door" src={theDoor} className="closet-door-right20" />
+            </div>
+            <div>
+                <img alt="Closet Door" src={theDoor} className="closet-door-right10" />
+            </div>
         </div>
     )
 }
